25d5161040cdc0bd98e3f13b61d03f45aa572d28
nickDevUic <nickDevUic@gmail.com>
2016-05-05 17:39:06 +0800

    Update SWRevealViewController.m
    Modify RevealWidth

--- SWRevealViewController/SWRevealViewController.m
+++ SWRevealViewController/SWRevealViewController.m
@@ -28,6 +28,7 @@
 
 #import "SWRevealViewController.h"
 
+#define SCREEN_WIDTH    [UIScreen mainScreen].bounds.size.width
 
 #pragma mark - StatusBar Helper Function
 
@@ -644,10 +645,10 @@ const int FrontViewPositionNone = 0xff;
     _frontViewPosition = FrontViewPositionLeft;
     _rearViewPosition = FrontViewPositionLeft;
     _rightViewPosition = FrontViewPositionLeft;
-    _rearViewRevealWidth = 260.0f;
+    _rearViewRevealWidth = SCREEN_WIDTH - 100.0f;
     _rearViewRevealOverdraw = 60.0f;
     _rearViewRevealDisplacement = 40.0f;
-    _rightViewRevealWidth = 260.0f;
+    _rightViewRevealWidth = SCREEN_WIDTH - 100.0f;
     _rightViewRevealOverdraw = 60.0f;
     _rightViewRevealDisplacement = 40.0f;
     _bounceBackOnOverdraw = YES;
